caa9a1b33bd1684eae853df0b50c4edeb4199a84
Bariscan Ceken <bariscanceken@hotmail.com>
2024-10-22 11:45:22 +0300

    os module and english.

--- Hash Converter/mainhashconverter.py
+++ Hash Converter/mainhashconverter.py
@@ -1,47 +1,50 @@
 import hashlib
+import os
 
 password_input = input("Neyi Şifrelemek İstersin?: ")
-with_which = int(input("Hangi Şifreleme Yöntemiyle Şifrelemek İstersin? |(1)md5,(2)sha1,(3)sha224,(4)sha256,(5)sha384,(6)sha512)| :  "))
-sifrelenecek = password_input
+with_which = input("Hangi Şifreleme Yöntemiyle Şifrelemek İstersin? |(1)md5,(2)sha1,(3)sha224,(4)sha256,(5)sha384,(6)sha512)| :  ")
 
-number = [1,2,3,4,5,6]
+number = ["1","2","3","4","5","6"]
 
 def convert() :
-    
-    global sifrelenecek
 
-    if with_which == 1 :
-            sifreleyici = hashlib.md5()
-            neyle = "md5"
+     global password_input
+
+     if with_which == "1" :
+          encryptor = hashlib.md5()
+          strfunc = "md5"
             
-    elif with_which == 2 :
-         sifreleyici = hashlib.sha1()
-         neyle = "sha1"
+     elif with_which == "2" :
+         encryptor = hashlib.sha1()
+         strfunc = "sha1"
          
-    elif with_which == 3 :
-         sifreleyici = hashlib.sha224()
-         neyle = "sha224"
+     elif with_which == "3" :
+         encryptor = hashlib.sha224()
+         strfunc = "sha224"
          
-    elif with_which == 4 :
-         sifreleyici = hashlib.sha256()
-         neyle = "sha256"
+     elif with_which == "4" :
+         encryptor = hashlib.sha256()
+         strfunc = "sha256"
          
-    elif with_which == 5 :
-         sifreleyici = hashlib.sha384()
-         neyle = "sha384"
+     elif with_which == "5" :
+         encryptor = hashlib.sha384()
+         strfunc = "sha384"
          
-    elif with_which == 6 :
-         sifreleyici = hashlib.sha512()
-         neyle = "sha512"
+     elif with_which == "6" :
+         encryptor = hashlib.sha512()
+         strfunc = "sha512"
          
-    while with_which not in number :
+     while with_which not in number :
         print("Hatalı Veri Girdiniz. Tekrar Deneyin!")
         convert()
 
-    sifreleyici.update(sifrelenecek.encode())
-    typestr = sifreleyici.hexdigest()
+     encryptor.update(password_input.encode())
+     typestr = encryptor.hexdigest()
 
-    with open("hashconverter.txt", "a") as file:
-        file.write(f"{sifrelenecek} ----> {neyle} ile sifrelenmis hali = {typestr}\n")
+     script_directory = os.path.dirname(os.path.abspath(__file__))
+     file_path = os.path.join(script_directory, "hashconverterlist.txt")
+
+     with open(file_path, "a") as file:
+          file.write(f"{password_input} ----> {strfunc} ile sifrelenmis hali = {typestr}\n")
 
 convert()
